bdb2c3bd1349e37c9262a209e2b0feebbfe92b71
JingYu <747515435@qq.com>
2024-04-02 22:33:51 +0800

    go to my github

---
+++ src/main/java/JdbcTest.java
@@ -0,0 +1,82 @@
+package main.java;
+
+import java.sql.*;
+
+public class JdbcTest {
+    private Connection conn = null;
+    private String url = "jdbc:sqlite:jy_demo.db";
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+    private ResultSetMetaData rm = null;
+
+    /**
+     * 连接数据库
+     *
+     */
+    public JdbcTest() throws Exception {
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection(url);
+    }
+
+    /**
+     * 连接
+     * @return
+     * @throws Exception
+     */
+    private Connection getConn() throws Exception {
+        if (conn == null) {
+            conn = DriverManager.getConnection(url);
+        }
+        return conn;
+    }
+
+    /**
+     * 关闭连接
+     * @param con
+     * @param ps
+     * @param rs
+     * @throws Exception
+     */
+    private void closeConn(Connection con, PreparedStatement ps, ResultSet rs) throws Exception {
+        if (rs != null) {
+            rs.close();
+            rs = null;
+        }
+        if (ps != null) {
+            ps.close();
+            ps = null;
+        }
+        if (con != null) {
+            con.close();
+            conn = null;
+        }
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        JdbcTest j = new JdbcTest();
+        String sql = "select * from jy_tbl";
+        Connection conn = j.getConn();
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        int l = rs.getMetaData().getColumnCount();
+        String columnName = null;
+        String columnTypeName = null;
+        int anInt = 0;
+        String str = null;
+        while (rs.next()) {
+            for (int i = 0; i < l; i++) {
+                columnName = rs.getMetaData().getColumnName(i + 1);
+                columnTypeName = rs.getMetaData().getColumnTypeName(i + 1);
+                if ("INTEGER".equals(columnTypeName)) {
+                    anInt = rs.getInt(columnName);
+                    System.out.println(columnName+"-"+columnTypeName+":"+anInt);
+                }else if ("CHAR".equals(columnTypeName)){
+                    str = rs.getString(columnName);
+                    System.out.println(columnName+"-"+columnTypeName+":"+str);
+                }
+            }
+        }
+    }
+}
